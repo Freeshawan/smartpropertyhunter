e95a1999a0e6c4f7d348ac9936634b60fd8ccea6
Freeshawan <mo@vanbellinghen.eu>
2025-04-28 09:53:11 +0200

    Delete src/App.js

--- src/App.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>SmartPropertyHunter</h1>
-      <p>Bienvenue dans votre moteur de recherche immobilier intelligent.</p>
-    </div>
-  );
-}
